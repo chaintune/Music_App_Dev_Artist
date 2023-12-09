400007736d4f7b8c7042c62de3f7ca71e5ad19d5
Tushar Banik <b.toshar@iitg.ac.in>
2023-12-09 18:14:42 +0530

    release card

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { ReleaseCard } from "@/components/ReleaseCard/ReleaseCard";
+
+const Dashboard = () => {
+    return ( 
+        <div className="h-screen w-screen flex items-center justify-center">
+            <ReleaseCard />
+        </div>
+     );
+}
+ 
+export default Dashboard;
