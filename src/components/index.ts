be1084fffa6a125af3d42269f42dbdeeda43fdfd
Saraswata-18 <saraswatasanyal@gmail.com>
2023-12-10 01:15:57 +0530

    Made Components Of Artist Landing Page

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,8 @@
+import ArtistCorner from './ArtistCorner/artistcorner';
+import ArtistPicks from './ArtistPicks/artistpicks';
+import AtAGlance from './AtAGlance/ataglance';
+import Community from './Community/community';
+
 export { default as CommunityCard } from './CommunityCard/CommunityCard';
 export { default as StatisticsCard } from './StatisticsCard/StatisticsCard';
 export { default as Navbar } from './Navbar/Navbar';
@@ -6,3 +11,7 @@ export { default as CreateCard } from './CreateReleaseCard/CreateRelease';
 export { default as Layout } from './Layout/Layout';
 export { default as AlbumsCard } from './TopAlbumsCard/AlbumsCard';
 export { default as EarningsCard } from './EarningsCard/EarningsCard';
+export { ArtistCorner};
+export {ArtistPicks};
+export {AtAGlance};
+export {Community};
