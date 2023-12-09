dc9898e9e63ad84972194d10e5f5982e7233898d
Tushar Banik <b.toshar@iitg.ac.in>
2023-12-09 23:45:06 +0530

    Login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-const Signup = () => {
+const Login = () => {
     return (
         <div className="h-screen w-screen bg-zinc-950 flex flex-row">
             <div id="NewRootRoot" className="flex flex-row w-[50vw] bg-white items-start">
@@ -15,12 +15,12 @@ const Signup = () => {
                 </div>
             </div>
 
-            <div className="flex flex-col gap-[1vw] py-[10vw] px-[10vw]">
+            <div className="flex flex-col gap-[2vw] py-[15vw] px-[10vw]">
                 <div className="text-4xl font-['Aileron'] font-light leading-[48px] text-white w-full">
                     Experience music like it should be.
                 </div>
                 <div className="opacity-60 font-['Aileron'] font-light leading-[22px] text-white">
-                    Already have an account? <a href="/login" className="cursor-pointer underline">Login here</a>
+                    Dont have an account? <a href="/signup" className="cursor-pointer underline">Signup here</a>
                 </div>
                 <div className="w-[35vw] pt-[0.7vw] border-solid border-white/6 backdrop-blur-[24px] shadow-[2px_4px_48px_0px_rgba(0,_0,_0,_0.5)] bg-[linear-gradient(159deg,_rgba(28,_30,_34,_0.33)_-9%,rgba(31,_34,_40,_0.5)_113%)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat flex flex-row justify-between h-16 items-start pl-6 pr-2 rounded-[24px]">
                     <div className="text-xl font-['Aileron'] font-light leading-[28px] text-white mt-2">
@@ -30,28 +30,13 @@ const Signup = () => {
                         Connect Wallet
                     </div>
                 </div>
-                <div className="w-[35vw] backdrop-blur-[24px] shadow-[2px_4px_48px_0px_rgba(0,_0,_0,_0.5)] bg-[linear-gradient(159deg,_rgba(28,_30,_34,_0.33)_-9%,rgba(31,_34,_40,_0.5)_113%)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat flex flex-col justify-center gap-2 items-start p-4 rounded-[24px]">
-                    <div className="flex flex-col gap-1 w-full items-start">
-                        <div className="opacity-60 text-xs font-['Aileron'] leading-[20px] text-white ml-1">
-                            Artist Name
-                        </div>
-                        <input className="outline-none text-sm font-['Aileron'] leading-[20px] text-white bg-[rgba(38,_40,_44,_0.4)] flex flex-row w-full h-12 items-start px-4 rounded-lg" placeholder="Nothing Happens #1" />
-                    </div>
-                    <div className="flex flex-col gap-1 w-full items-start">
-                        <div className="opacity-60 text-xs font-['Aileron'] leading-[20px] text-white ml-1">
-                            Description
-                        </div>
-                        <textarea className="outline-none text-sm font-['Aileron'] leading-[20px] text-white bg-[rgba(38,_40,_44,_0.4)] flex flex-row w-full h-24 items-start pt-3 px-4 rounded-lg" placeholder="Enter description here..." />
-                    </div>
-                </div>
                 <div className="w-[35vw] border-solid border-white/6 backdrop-blur-[24px] bg-[linear-gradient(#ffffff,_#ffffff),_linear-gradient(159deg,_rgba(28,_30,_34,_0.33)_-9%,rgba(31,_34,_40,_0.5)_113%)] bg-cover,_cover bg-50%_50%,_50%_50% bg-blend-normal,_normal bg-repeat-no-repeat,_no-repeat flex flex-row justify-center pt-3 h-12 items-start border rounded-[24px]">
-                    <div className="text-sm font-['Aileron'] leading-[20px]">Create Account</div>
+                    <div className="text-sm font-['Aileron'] leading-[20px]">Login</div>
                 </div>
             </div>
 
         </div>
-
     );
 }
 
-export default Signup;
+export default Login;
