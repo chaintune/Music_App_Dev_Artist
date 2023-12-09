ed0c7f191178ef2085a478ddb3848747a56305c1
Tushar Banik <b.toshar@iitg.ac.in>
2023-12-09 19:29:10 +0530

    Album card

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,15 @@
 import CommunityCard from "@/components/CommunityCard/CommunityCard";
 import { CreateCard  } from "@/components/CreateReleaseCard/CreateRelease";
 import { ManageCard } from "@/components/ManageReleaseCard/ManageRelease";
+import AlbumsCard from "@/components/TopAlbumsCard/AlbumsCard";
 
 const Dashboard = () => {
     return ( 
         <div className="h-screen w-screen flex items-center justify-center">
-            <CreateCard />
+            {/* <CreateCard />
             <ManageCard />
-            <CommunityCard />
+            <CommunityCard /> */}
+            <AlbumsCard />
         </div>
      );
 }
