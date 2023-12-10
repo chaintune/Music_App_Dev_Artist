29a6dbc9faec9af246c539840b22269c46f1fb42
Tushar Banik <b.toshar@iitg.ac.in>
2023-12-10 13:29:54 +0530

    example api route

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -1,11 +1,21 @@
 import mongoose from "mongoose";
 
 const mongooseconnect = async () => {
-    if (mongoose.connections[0].readyState === 1)
-        return mongoose.connections[0].asPromise();
+    // if (mongoose.connections[0].readyState === 1)
+    //     return mongoose.connections[0].asPromise();
+    try {
+        const uri = process.env.MONGODB_URI as string;
+        await mongoose.connect(uri,{
+            dbName: "Music_App",
+        });
+        console.log("Connected to MongoDB");
+    }
+    catch (err) {
+        console.error(err);
+        console.log("Failed to connect to MongoDB");
+    }
 
-    const uri = process.env.MONGODB_URI as string;
-    return mongoose.connect(uri);
+    
 };
 
 export default mongooseconnect;
