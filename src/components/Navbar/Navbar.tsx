3d845d29e055fdb2579451b38002ad87bce3ced3
Atharvat <tagalpallewaratharva@gmail.com>
2023-12-11 22:15:40 +0530

    Navbar UI fixes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,22 +8,21 @@ const Navbar = () => {
     const pathname = usePathname();
   
     return (
-        <div className="flex bg-zinc-950 py-[1.5vw] px-[1.5vw] fixed z-10 flex-row justify-between w-full items-start">
-            <div className="flex flex-row gap-6 w-2/5 items-start">
+        <div className="flex bg-zinc-950 py-[1.5vw] px-[1.5vw] fixed z-10 flex-row justify-between w-full items-center">
+            <div className="flex flex-row items-center justify-start">
                 <div
                     id="Ellipse6"
-                    className="bg-[url(https://file.rendit.io/n/EqzVshX3tL42A3YRxuql.svg)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat relative flex-row justify-center mt-px w-12 h-12"
+                    className="bg-[url(https://file.rendit.io/n/EqzVshX3tL42A3YRxuql.svg)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat relative flex flex-row justify-center mt-px w-12 h-12 items-center -left-5"
                 >
                     <img
                         src="https://file.rendit.io/n/yGydWNJcxTGpgcriKI8K.svg"
                         alt="Vector2"
-                        className="w-3 h-3 origin-top-left rotate-[-0deg] absolute top-[1.2vw] left-[1.2vw]"
+                        className="w-[17px] h-[17px] origin-top-left rotate-[-0deg]"
                     />
                 </div>
-                <div className="flex flex-row justify-between w-5/6 items-start">
+                <div className="flex flex-row justify-between gap-3 items-center">
                     <a  href="/" className={`text-4xl font-['Aileron'] font-light leading-[48px] text-white ${pathname === '/' ? 'opacity-100' : 'opacity-30'} cursor-pointer`}>
                         Home
-              
 
                     </a>
                     <a href="/createRelease" className={`text-4xl font-['Aileron'] font-light leading-[48px] text-white ${pathname === '/createRelease' ? 'opacity-100' : 'opacity-30'} cursor-pointer`}>
@@ -37,26 +36,26 @@ const Navbar = () => {
                         src="https://file.rendit.io/n/0qOa1cpotCzSOnIbUhn0.svg"
                         alt="Search"
                         id="Search"
-                        className="mt-1 w-10"
+                        className="w-10"
                     />
                 </div>
             </div>
-            <div className="flex flex-row justify-between w-1/2 items-start">
-                {/* <div className="border-solid border-white/6 backdrop-blur-[24px] shadow-[2px_4px_48px_0px_rgba(0,_0,_0,_0.5)] bg-[linear-gradient(159deg,_rgba(28,_30,_34,_0.33)_-9%,rgba(31,_34,_40,_0.5)_113%)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat flex flex-row justify-end gap-10 w-1/4 items-start pt-1 px-1 border rounded-[24px]">
-                    <div className="flex flex-row mt-3 gap-2 w-16 items-start">
+            <div className="flex flex-row justify-between items-center gap-4">
+                {/* <div className="backdrop-blur-[24px] shadow-[2px_4px_48px_0px_rgba(0,_0,_0,_0.5)] bg-[linear-gradient(159deg,_rgba(28,_30,_34,_0.33)_-9%,rgba(31,_34,_40,_0.5)_113%)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat flex flex-row justify-end gap-10 items-center p-1 rounded-[24px]">
+                    <div className="flex flex-row gap-2 w-16 items-center">
                         <img
                             src="https://file.rendit.io/n/biEAQlVezNJF0nwE6YQ7.svg"
                             alt="Ellipse5"
                             id="Ellipse5"
-                            className="w-6"
+                            className="w-6 ml-2"
                         />
-                        <div className="font-['Aileron'] font-light leading-[24px] text-white">
+                        <div className="font-['Aileron'] font-light text-white">
                             0
                         </div>
                     </div>
                     <div
                         id="Ellipse4"
-                        className="bg-[url(https://file.rendit.io/n/PUBsjlM60pCu0Nr1DFsn.svg)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat flex flex-row justify-center mb-1 pt-4 w-12 h-12 items-start"
+                        className="bg-[url(https://file.rendit.io/n/PUBsjlM60pCu0Nr1DFsn.svg)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat flex flex-row justify-center w-12 h-12 items-center"
                     >
                         <img
                             src="https://file.rendit.io/n/2MsiJ95iJMNwJITgevH6.svg"
@@ -65,13 +64,13 @@ const Navbar = () => {
                         />
                     </div>
                 </div> */}
-                <div className="border-solid border-white/6 backdrop-blur-[24px] shadow-[2px_4px_48px_0px_rgba(0,_0,_0,_0.5)] bg-[linear-gradient(159deg,_rgba(28,_30,_34,_0.33)_-9%,rgba(31,_34,_40,_0.5)_113%)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat flex flex-row justify-end gap-10 w-1/4 items-start pt-1 px-1 border rounded-[24px]">
-                    <div className="flex flex-row mt-3 gap-2 w-16 items-start">
+                <div className="backdrop-blur-[24px] shadow-[2px_4px_48px_0px_rgba(0,_0,_0,_0.5)] bg-[linear-gradient(159deg,_rgba(28,_30,_34,_0.33)_-9%,rgba(31,_34,_40,_0.5)_113%)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat flex flex-row justify-end gap-10 items-center p-1 rounded-[24px]">
+                    <div className="flex flex-row gap-2 w-16 items-center">
                         <img
                             src="https://file.rendit.io/n/biEAQlVezNJF0nwE6YQ7.svg"
                             alt="Ellipse3"
                             id="Ellipse3"
-                            className="w-6"
+                            className="w-6  ml-2"
                         />
                         <div className="font-['Aileron'] font-light leading-[24px] text-white">
                             0
@@ -79,7 +78,7 @@ const Navbar = () => {
                     </div>
                     <div
                         id="Ellipse2"
-                        className="bg-[url(https://file.rendit.io/n/bH6aHaz0mgBlxNORTOAL.svg)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat flex flex-row justify-center mb-1 pt-4 w-12 h-12 items-start"
+                        className="bg-[url(https://file.rendit.io/n/bH6aHaz0mgBlxNORTOAL.svg)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat flex flex-row justify-center w-12 h-12 items-center"
                     >
                         <img
                             src="https://file.rendit.io/n/2MsiJ95iJMNwJITgevH6.svg"
@@ -88,23 +87,23 @@ const Navbar = () => {
                         />
                     </div>
                 </div>
-                <div className="border-solid border-white/6 backdrop-blur-[24px] shadow-[2px_4px_48px_0px_rgba(0,_0,_0,_0.5)] bg-[linear-gradient(159deg,_rgba(28,_30,_34,_0.33)_-9%,rgba(31,_34,_40,_0.5)_113%)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat flex flex-row justify-end gap-10 w-2/5 items-start pt-1 px-1 border rounded-[24px]">
-                    <div className="opacity-60 font-['Aileron'] font-light leading-[24px] text-white mt-3">
+                <div className="backdrop-blur-[24px] shadow-[2px_4px_48px_0px_rgba(0,_0,_0,_0.5)] bg-[linear-gradient(159deg,_rgba(28,_30,_34,_0.33)_-9%,rgba(31,_34,_40,_0.5)_113%)] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat flex flex-row justify-end gap-10 items-center p-1 rounded-[24px]">
+                    <div className="opacity-60 font-['Aileron'] font-light text-white ml-4">
                         0x22...5940
                     </div>
-                    <div className="flex flex-row mb-1 gap-1 w-1/2 items-start">
-                        <div className="relative flex flex-row justify-end w-20 items-start">
+                    <div className="flex flex-row gap-1 items-center">
+                        <div className="relative flex flex-row justify-end items-center">
                             <img
                                 src="https://file.rendit.io/n/zVBXz0gOc7Bzo9l04bHw.svg"
                                 alt="Ellipse1"
                                 id="Ellipse1"
-                                className="w-12 h-12 absolute top-0 left-0"
+                                className="w-12 h-12"
                             />
                             <img
                                 src="https://file.rendit.io/n/OO5UyTns3J8fdzqXQaXE.svg"
-                                alt="Subtract"
-                                id="Subtract"
-                                className="relative w-10"
+                                alt="Ellipse2"
+                                id="Ellipse2"
+                                className="w-12 h-12 -translate-x-1"
                             />
                         </div>
                         <img
