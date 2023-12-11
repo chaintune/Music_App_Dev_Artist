8abc252604dcc24b532c6c5acd37713c6015abaa
Tushar Banik <b.toshar@iitg.ac.in>
2023-12-11 21:03:25 +0530

    fix Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 'use client'
+import TopTracks from '@/components/topTracksCard/TopTracks'
+import NFTCollection from '@/components/nftCard/NFTCollection'
+import ArtistBar from "@/components/ArtistBar"
+import DiscoverArtist from "@/components/discoverArtistCard/discoverArtist"
 
 const Home_page = () => {
-    return (
-      <div>
-        home page
-      </div>
-    );
-  }
-
-  export default Home_page;
+  return ( 
+    <div>
+      {/* <ArtistBar/>
+      <TopTracks/>
+      <NFTCollection/>
+      <DiscoverArtist/> */}
+      home page
+    </div>
+   );
+}
+ 
+export default Home_page;
