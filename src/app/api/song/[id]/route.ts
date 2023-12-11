3c30facbf2d9b6ed1d24690e2167ad69004df59b
Tushar Banik <b.toshar@iitg.ac.in>
2023-12-11 13:46:55 +0530

    update song api

--- src/app/api/song/[id]/route.ts
+++ src/app/api/song/[id]/route.ts
@@ -1,18 +1,23 @@
 import { Song } from "@/models/song";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest } from "next/server";
 
 export const getDiscussions = async (
-    req: NextApiRequest,
-    res: NextApiResponse
+    req: NextRequest,
 ) => {
-    const { id: song_id } = req.query;
+    const song_id = await req.nextUrl.searchParams.get("query");
 
     try {
         const song = await Song.find({ song_id });
         console.log(`The song data the api sending is ${song}`);
-        return res.status(200).json(song);
+        return new Response(JSON.stringify(song), {
+            status: 200,
+            statusText: "Success",
+        });
     } catch (err) {
         console.log(err);
-        return res.status(400).json({ message: "Error fetching discussion" });
+        return new Response(JSON.stringify({ message: "Error fetching song" }), {
+            status: 400,
+            statusText: "Error",
+        });
     }
 };
