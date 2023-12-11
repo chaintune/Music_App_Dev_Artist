9321a6f4aa029636f860fa07fed841ff8dc42580
Himanshi <ghimanshi941@gmail.com>
2023-12-11 12:19:27 +0530

    fixed create song api

--- src/app/api/song/new/route.ts
+++ src/app/api/song/new/route.ts
@@ -3,11 +3,11 @@ import pinataSDK from '@pinata/sdk';
 import { connectToDB } from '@/utils/database';
 const pinata = new pinataSDK({ pinataJWTKey: process.env.NEXT_PUBLIC_JWT });
 import { Song } from '@/models/song';
-import Date from '../../../../components/date'
 
 export async function POST(req: Request) {
   try {
     const { song, album, creator, imageCid, artist, audioCid, genre,license, language } = await req.json();
+    const currentDate = new Date();
     await connectToDB()
     let newJsonBody = {
       name: song,
@@ -34,7 +34,7 @@ export async function POST(req: Request) {
         },
         {
           trait_type: 'Posting Date',
-          value: Date,
+          value: currentDate.toISOString(),
         },
         {
           trait_type: 'Language',
@@ -68,7 +68,7 @@ export async function POST(req: Request) {
 
     const pinataResult = await pinata.pinJSONToIPFS(newJsonBody, options);
     const hash = pinataResult.IpfsHash;
-
+    
     newJsonBody = {
       name: song,
       album: album,
@@ -94,7 +94,7 @@ export async function POST(req: Request) {
         },
         {
           trait_type: 'Posting Date',
-          value: Date,
+          value: currentDate.toISOString(),
         },
         {
           trait_type: 'Language',
@@ -118,6 +118,8 @@ export async function POST(req: Request) {
     // Save the data to MongoDB
     const savedSong = await Song.create({
       name: newJsonBody.name,
+      album: newJsonBody.album,
+      creator: newJsonBody.creator,
       image: newJsonBody.image,
       animation_url: newJsonBody.animation_url,
       attributes: newJsonBody.attributes,
